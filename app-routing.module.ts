e88eb742659180cd06f33d82b6ad366ecb95e790
brendontiver <btiver7@gmail.com>
2019-03-30 22:54:15 -0400

    Homework 2
    Changes made to app.module.ts, app-routing.modulte.ts.  
    Added sign-up.component.html, sign-up.component.ts

---
+++ app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './common/auth/login.component';
+import { SignUpComponent } from './common/auth/sign-up.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '**', component: HomeComponent },
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(appRoutes)],
+})
+export class AppRoutingModule {}
